feaf73dd98702e1ace3795d40df3adc8f9d95917
Dan Kiss and John Parsons <dan@>
2017-08-10 15:49:39 -0700

    complete business and front end and fix business logic final total

--- js/scripts.js
+++ js/scripts.js
@@ -3,23 +3,28 @@ $(document).ready(function() {
 
   $("form#questions").submit(function(event) {
     event.preventDefault();
-    var total = $("input:radio[name=question1]:checked").val();
+    var total1 = parseInt($("input:radio[name=question1]:checked").val());
+    var total2 = parseInt($("input:radio[name=question2]:checked").val());
+    var favoriteColor = $("#color").val();
 
-    $("#output").text(total);
+    var total3 = $("#dob").val();
+
+    $("#output").text("Your date is set on " + total3);
+
+    var finaltotal = total1+total2;
+
+    if (finaltotal >= 5) {
+      $('.kesha').show();
+    } else if (finaltotal >=3 && finaltotal <5) {
+      $('.betty').show();
+    } else if (finaltotal ===2){
+      $('.trump').show();
+    }
+
+
+    document.body.style.backgroundColor = favoriteColor;
+
   });
+
+
 });
-
-// function(red){
-//   alert("hello!"");
-// }
-
-// {
-//   alert('hello world');
-// });
-//
-//
-// });
-
-
-
-// alert('This is working!');
