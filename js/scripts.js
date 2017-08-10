29a97a65fcd81ab999bd2cf32acff117ffa4229c
Dan Kiss and John Parsons <dan@>
2017-08-10 14:23:21 -0700

    create business side of quiz

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,25 @@
 // var flavor = $("input:radio[name=flavor]:checked").val(); reading value
+$(document).ready(function() {
+
+  $("form#questions").submit(function(event) {
+    event.preventDefault();
+    var total = $("input:radio[name=question1]:checked").val();
+
+    $("#output").text(total);
+  });
+});
+
+// function(red){
+//   alert("hello!"");
+// }
+
+// {
+//   alert('hello world');
+// });
+//
+//
+// });
+
+
+
+// alert('This is working!');
